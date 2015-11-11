52cfd0ac9061ffb50435408a0b1c1f24e21232bb
Charles Watson <nchlswtsn@gmail.com>
2015-11-11 15:41:51 -0800

    clean out edit mistakes in

--- src/js/module.js
+++ src/js/module.js
@@ -8,6 +8,7 @@ app.config(function($stateProvider, $urlRouterProvider){
   $stateProvider
     .state('home', { url: '/', templateUrl: '/html/general/home.html', controller: 'homeCtrl' })
 
+
     .state('users', { abstract: true, templateUrl: '/html/users/users.html'})
     .state('users.login', { url: '/login', templateUrl: '/html/users/form.html', controller: 'usersCtrl'})
 
