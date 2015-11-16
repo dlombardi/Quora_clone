3459b45bf55173b91baf834dba2534d358b016a4
Darien Lombardi <darien.lombardi@gmail.com>
2015-11-16 14:39:19 -0800

    make images gulp task and fixed like and unlike per post feature

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,14 @@ var dirs = {
   src: {
     scss: './src/stylesheets/*.scss',
     js: "./src/javascripts/**/*.js",
-    html: "./src/templates/***/**/*.html"
+    html: "./src/templates/***/**/*.html",
+    img: "./src/images/**/*.*"
   },
   dist: {
     css: './public/css',
     js: './public/js',
-    html: './public/html'
+    html: './public/html',
+    img: './public/images'
   }
 };
 
@@ -40,10 +42,16 @@ gulp.task('html', function(){
     .pipe(gulp.dest(dirs.dist.html));
 });
 
+gulp.task('images', function(){
+  return gulp.src(dirs.src.img)
+    .pipe(gulp.dest(dirs.dist.img));
+});
+
 gulp.task('watch', function(){
   gulp.watch(dirs.src.js, ['scripts']);
   gulp.watch(dirs.src.scss, ['sass']);
   gulp.watch(dirs.src.html, ['html']);
+  gulp.watch(dirs.src.img, ['images']);
 });
 
-gulp.task('default', ['watch', 'sass', 'scripts', 'html']);
+gulp.task('default', ['watch', 'sass', 'scripts', 'html', "images"]);
