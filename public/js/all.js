38ce701b963427cf420b70d18dc19ab971e420e6
Charles Watson <nchlswtsn@gmail.com>
2015-11-16 22:46:33 -0800

    add questions post form and controller logic

--- public/js/all.js
+++ public/js/all.js
@@ -165,10 +165,43 @@ app.controller('usersCtrl', function($scope, $state, auth, userFactory, $rootSco
 
 'use strict';
 
-app.controller('writeCtrl', function($scope, $state, postFactory, auth){
-  console.log("WRITE CTRL WORKING");
 
 
+app.controller('writeCtrl', function($scope, $http){
+  var posts = [];
+  $(document).foundation();
+
+  $http({
+    method: 'GET',
+    url: '/topics'
+  }).then(function(data){
+    var specialdata = [{myButt: 'data'}, {myButt: 'butt'}, {myButt: 'rocks'}, {myButt: 'salt'}, {myButt: 'bathsalt'}];
+    console.log('data is: ', data);
+    $scope.topics = specialdata;
+  }).catch(function(err){
+    console.error('errthang is wrong.', err, status);
+  });
+
+
+  $scope.submitQuestion = function(question){
+    posts.push({
+      title: question.title,
+      tags: question.tags,
+      details: question.content,
+      topic: question.topic
+    });
+    $scope.question = {};
+    console.log(posts);
+
+    $http({
+      method: 'POST',
+      url: '/posts/add'
+    }).then(function(data){
+      console.log(data);
+    }).catch(function(err){
+      console.error("Error saving question.");
+    });
+  }
 });
 
 'use strict';
--- src/javascripts/controllers/writeCtrl.js
+++ src/javascripts/controllers/writeCtrl.js
@@ -1,7 +1,40 @@
 'use strict';
 
-app.controller('writeCtrl', function($scope, $state, postFactory, auth){
-  console.log("WRITE CTRL WORKING");
 
 
+app.controller('writeCtrl', function($scope, $http){
+  var posts = [];
+  $(document).foundation();
+
+  $http({
+    method: 'GET',
+    url: '/topics'
+  }).then(function(data){
+    var specialdata = [{myButt: 'data'}, {myButt: 'butt'}, {myButt: 'rocks'}, {myButt: 'salt'}, {myButt: 'bathsalt'}];
+    console.log('data is: ', data);
+    $scope.topics = specialdata;
+  }).catch(function(err){
+    console.error('errthang is wrong.', err, status);
+  });
+
+
+  $scope.submitQuestion = function(question){
+    posts.push({
+      title: question.title,
+      tags: question.tags,
+      details: question.content,
+      topic: question.topic
+    });
+    $scope.question = {};
+    console.log(posts);
+
+    $http({
+      method: 'POST',
+      url: '/posts/add'
+    }).then(function(data){
+      console.log(data);
+    }).catch(function(err){
+      console.error("Error saving question.");
+    });
+  }
 });
