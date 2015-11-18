689eb293802f7865fdcc1a794c804121f5acb1c1
Samantha Degges <samanthadegges@gmail.com>
2015-11-17 22:32:27 -0800

    Wireframe feed, follow and edit

--- src/javascripts/controllers/profileCtrl.js
+++ src/javascripts/controllers/profileCtrl.js
@@ -12,7 +12,7 @@ app.controller('profileCtrl', function($scope, $state, auth){
     work: String,
     about: "I'm a Idaho transplant. I like protein power, casein powder, and the powder of any prototypes. The more powder, the better. You are what you eat and I am powder. I put the POW in powder. I also put the OW in powder. I also eat flax seeds for fiber content.",
     subscriptions: ['cars', 'cats', 'ferrets'],
-    knowledge: [],
+    knowledge: ['bowling', 'Abyssian cats', 'rock collecting', 'muppets', 'protein'],
     followers: 34,
     following: 79,
     posts: [],
