8d6ebddd4ea9f8635d76e120a9fc90a894614d80
Samantha Degges <samanthadegges@gmail.com>
2015-11-18 12:53:59 -0800

    committing public before merging local master to local branch.

--- public/js/all.js
+++ public/js/all.js
@@ -131,8 +131,39 @@ app.controller('navCtrl', function($scope, $state, auth, postFactory){
 
 
 app.controller('profileCtrl', function($scope, $state, auth){
+  $(document).foundation();
   console.log("PROFILE CTRL WORKING");
+
+  $scope.user = {
+    username:  "bananaLeaf31",
+    fullName: "Firstname Lastname",
+    email: String,
+    home: 'Boise, Idaho',
+    work: String,
+    about: "I'm a Idaho transplant. I like protein power, casein powder, and the powder of any prototypes. The more powder, the better. You are what you eat and I am powder. I put the POW in powder. I also put the OW in powder. I also eat flax seeds for fiber content.",
+    subscriptions: ['cars', 'cats', 'ferrets'],
+    knowledge: ['bowling', 'Abyssian cats', 'rock collecting', 'muppets', 'protein'],
+    followers: 34,
+    following: 79,
+    posts: [],
+    postLikes: [],
+    likes: 0,
+    views: 0,
+    hash: String,
+  }
+
+  $scope.follow = function(){
+    console.log("follow clicked.");
+  }
+
 });
+
+// app.service("editUserByPost", function($http){
+//   var postUser = function(){
+//     // topic.name =
+//     // user.
+//   }
+// });
 
 'use strict';
 
